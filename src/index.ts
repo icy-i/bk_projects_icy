7fe8f1efd96413cd5d1aed4bf357097f46afc58b
icy-i <shngd.lksh@gmail.com>
2026-04-24 10:31:05 +0530

    feat: update greeting message and add user endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,36 @@ import express from "express";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
-const hey = "Hello World!";
+const hey = "Hello Lokesh!";
 
+const users = [
+    { id: 1, name: "A", email: "alice@example.com" },
+    { id: 2, name: "ok", email: "ok@example.com" },
+    { id: 3, name: "3", email: "charlie@example.com" },
+];
 app.get("/",(_req,res)=>{
     res.send(hey);
 });
 
+app.get("/users",(_req,res)=>{
+    res.json(users);
+})
+
+app.get("/users/:id",(req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((u)=> u.id === id);
+
+    if(user){
+        res.json(user);
+    } else {
+        res.status(404).send("User not found");
+    }
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("Available endpoints:");
+    console.log(`GET http://localhost:${PORT}/ - Returns a greeting message`);
+    console.log(`GET http://localhost:${PORT}/users - Returns a list of users`);
+    console.log(`GET http://localhost:${PORT}/users/:id - Returns a user by ID`);
 });
